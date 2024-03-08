e6af4573d5ab2cd497b5282be8502118fcbcdff4
rfzhfrn <5026231127@student.its.ac.id>
2024-03-08 08:02:02 +0700

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!, Hi Rafi Zhafran Putra Wibowo, Programming is so much fun!");
+        System.out.println("helo");
     }
 }
